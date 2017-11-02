6bba967c3d67594c99f376c6a810c41d49113a81
maso <mostafa.barmshory@dpq.co.ir>
2017-11-02 23:02:48 +0330

    Adding an empty test

--- tests/SuperTenant/ApiTest.php
+++ tests/SuperTenant/ApiTest.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of Pluf Framework, a simple PHP Application Framework.
  * Copyright (C) 2010-2020 Phoinex Scholars Co. (http://dpq.co.ir)
@@ -17,14 +16,29 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+use PHPUnit\Framework\TestCase;
 
-/**
- * Main view
- *
- * Manage tenants and offers basics of tenant
- *
- * @author maso<mostafa.barmshory@dpq.co.ir>
- */
-class SuperTenant_Views
+require_once 'Pluf.php';
+
+class SuperTenant_Api extends TestCase
 {
+
+    /**
+     * @before
+     */
+    public function setUp()
+    {
+        Pluf::start(dirname(__FILE__) . '/../conf/config.mysql.php');
+    }
+
+    /**
+     * @test
+     */
+    public function testClassInstance()
+    {
+//         $c = new CMS_Content();
+//         $this->assertTrue(isset($c));
+        $this->assertTrue(true);
+    }
 }
+
