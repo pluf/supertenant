e2374762d53ae6800ac6fed7842b8fe22a699085
hadi <hadi@dpq.co.ir>
2017-12-30 22:10:01 +0330

    remove 'unique' from tenant field of entities in SuperTenant

--- src/SuperTenant/Ticket.php
+++ src/SuperTenant/Ticket.php
@@ -40,7 +40,6 @@ class SuperTenant_Ticket extends Tenant_Ticket
                 'type' => 'Pluf_DB_Field_Foreignkey',
                 'model' => 'Pluf_Tenant',
                 'blank' => false,
-                'unique' => true,
                 'editable' => false
             )
         ));
